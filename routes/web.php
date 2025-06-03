8c79dfb0b8b8a5b2f3e502f565ba795ccd169f1f
Ephesians Prismaranatha <ephesiansprismaranatha@gmail.com>
2025-06-03 16:49:56 +0700

    upload clone

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\Obat;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ObatController;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware(['role:dokter'])->prefix('dokter')->group(function () {
+        Route::get('/', function () {
+            return view('dokter.dashboard');
+        })->name('dokter.dashboard');
+
+        Route::prefix('obat')->group(function () {
+            Route::get('/', [ObatController::class, 'index'])->name('dokter.obat.index');
+            Route::get('/create', [ObatController::class, 'create'])->name('dokter.obat.create');
+            Route::post('/store', [ObatController::class, 'store'])->name('dokter.obat.store');
+
+            Route::get('/edit/{id}', [ObatController::class, 'edit'])->name('dokter.obat.edit');
+            Route::put('/update/{id}', [ObatController::class, 'update'])->name('dokter.obat.update');
+
+            Route::delete('destroy/{id}', [ObatController::class, 'destroy'])->name('dokter.obat.destroy');
+
+
+        });
+
+    });
+});
+
+require __DIR__.'/auth.php';
