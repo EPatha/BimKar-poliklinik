052831354fc25e871acad12c3b4f651f46f609bb
Ephesians Prismaranatha <ephesiansprismaranatha@gmail.com>
2025-06-11 14:11:51 +0700

    update Janji Periksa

--- resources/views/pasien/janji/create.blade.php
+++ resources/views/pasien/janji/create.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>Add commentMore actions
+<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             Buat Janji Periksa
@@ -13,7 +13,11 @@
                     <select name="id_jadwal_periksa" required class="w-full border-gray-300 rounded p-2">
                         <option value="">-- Pilih Jadwal --</option>
                         @foreach($jadwal as $j)
-                            <option value="{{ $j->id }}">{{ $j->hari }} - {{ $j->jam_mulai }} s/d {{ $j->jam_selesai }}</option>
+                            <option value="{{ $j->id }}">
+                                {{ $j->dokter->nama ?? '-' }} | 
+                                {{ $j->dokter->poli ?? '-' }} | 
+                                {{ $j->hari }} - {{ $j->jam_mulai }} s/d {{ $j->jam_selesai }}
+                            </option>
                         @endforeach
                     </select>
                 </div>
@@ -22,8 +26,8 @@
                     <input type="text" name="keluhan" required class="w-full border-gray-300 rounded p-2" placeholder="Masukkan keluhan Anda">
                 </div>
                 <div class="flex justify-end">
-                <a href="{{ route('pasien.janji.index') }}" class="mr-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition">Batal</a>
-                 <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Simpan</button>
+                    <a href="{{ route('pasien.janji.index') }}" class="mr-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition">Batal</a>
+                    <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Simpan</button>
                 </div>
             </form>
         </div>
